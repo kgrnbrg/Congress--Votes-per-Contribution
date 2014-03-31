3b1e9c06ae1772db7fc460a9ec11f66a401f767b
Brian Clifton <briancliftonstudio@gmail.com>
2014-03-31 00:38:05 -0400

    Added beginning of Legislator Profile panel

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
-var oldDataset;
+var filteredDataset;
 var dataset = [];
 var name;
+var allLegislators;
 
-var catOrderObject = {};
-catOrderObject.catOrder = [];
-catOrderObject.catCode = [];
-
-
-var counter = 0;
 
 
 // Thank you for the snippet MREDKJ! http://www.mredkj.com/javascript/nfbasic.html
@@ -24,76 +19,65 @@ function addCommas(nStr)
 	return x1 + x2;
 }
 
-
 function getCongressMemberData(name){
-	console.log(name);
 
-	$.ajax({
-		url: 'https://www.govtrack.us/api/v2/person?q=' + name,
-		type: 'GET',
-		dataType: 'json',
-	})
-	.done(function(data) {
-		console.log("success");
-		console.log(data);
-		console.log("bioguideid: " + data.objects[0].bioguideid);
+	d3.csv('../assets/legislators.csv', function(error, data) {
+		if (error){
+			console.log(error);
+		} else {
+			console.log("[getCongressMemberData] name: " + name);
+			allLegislators = data;
 
-		$('.memberProfile').html('<img src="../img/100x125/' + data.objects[0].bioguideid + '.jpg" /><h3>' + data.objects[0].firstname + ' ' + data.objects[0].lastname + '</h3>');
+			var nameArray = name.split(' ');
+			var firstName = nameArray[0];
+			var lastName = nameArray[1];
 
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
+			var legislator = _.where(allLegislators, {firstname: firstName, lastname: lastName})
+			console.log(legislator);
+
+			$('#legislatorPhoto').html('<img src="../img/100x125/' + legislator[0].bioguide_id + '.jpg" />');
+			$('#legislatorName').text(name);
+			$('#legislatorState').text(legislator[0].state);
+			$('#legislatorChamber').text(legislator[0].title);
+		}
 	});
 	
 }
 
-
-
-////// Implement a way to use this for all legislators:
-// d3.csv("../assets/money-votes_All.csv", function(error, data) {
-// 	if (error){
-// 		console.log(error);
-// 	} else {
-// 		console.log(data);
-// 	}
-// });
+///////////////////////////////////////////////////////////////
+// var catOrderObject = {};
+// catOrderObject.catOrder = [];
+// catOrderObject.catCode = [];
 
 // function processCRPCategories() {
-
 // 	d3.csv('../assets/CRP_Categories.csv', function(error, data) {
-
 // 		if (error) {
 // 			console.log(error);
 // 		} else {
 // 			// console.log(data);
-
 // 			for(var i = 0; i < data.length; i++) {
 // 				catOrderObject.catOrder.push({
 // 					id: data[i]["Catorder"],
 // 					option: data[i]["Catcode"]
 // 				});
-
-
 // 			}
 // 				console.log(catOrderObject.catCode);
 // 				console.log(catOrderObject.catOrder);
-
-
-
 // 		}
 // 	});
-
 // }
+///////////////////////////////////////////////////////////////
+
+
 
 function drawGraphic(dataset){
-	var w = 960;
-	var h = 500;
-	var padding = 75;
 
-	// Sets the scale of the graphic, using sqrt() to show more detail with the smaller values, and less detail with the larger values. An attempt at making the graphic more legible.:
+	var w = $("#svg").width();		// This gets the width set by css. This will make the graphic repsonsive in the future.
+	var h = $("#svg").height();		// same
+	var padding = w/10;
+	// var svgTextSize = ...;
+
+	// Sets the scale of the graphic, using sqrt() to show more detail with the smaller values, and less detail with the larger values, making the data-heavy portion of the graphic more legible:
 	var xScale = d3.scale.sqrt()
 		.exponent(0.4)
 		.domain([0, d3.max(dataset, function(d){ return d[1]; })])
@@ -130,6 +114,7 @@ function drawGraphic(dataset){
 		.attr("width", w)
 		.attr("height", h);
 
+	// draws the axes of the graphic:
 	var xAxis = d3.svg.axis()
 		.scale(xScale)
 		.orient("bottom")
@@ -140,8 +125,6 @@ function drawGraphic(dataset){
 		.orient("left")
 		.ticks(10);
 
-
-	var color = 255;
 	var votesPerDollar;
 
 	svg.selectAll("circle")
@@ -157,10 +140,10 @@ function drawGraphic(dataset){
 
 				votesPerDollar = rScaleX(d[1]/scaleY(d[0]));
 
-				if (isNaN(votesPerDollar) || Number.isNaN(votesPerDollar) || votesPerDollar === Infinity){
+				if (isNaN(votesPerDollar) || Number.isNaN(votesPerDollar) || votesPerDollar === Infinity || votesPerDollar === -Infinity){
 					return 4;
 				} else if (votesPerDollar < 0){
-					console.log(votesPerDollar);
+					console.log("[circle:radius] Votes Per Dollar: " + votesPerDollar);
 					return Math.abs(votesPerDollar);
 				} else {
 					return votesPerDollar;
@@ -170,11 +153,11 @@ function drawGraphic(dataset){
 
 				votesPerDollar = rScaleX(d[1]/scaleY(d[0]));
 
-				if (isNaN(votesPerDollar) || Number.isNaN(votesPerDollar) || votesPerDollar === Infinity){
-					return "rgba(" + 4 + ", 0, 0, 0.2)";
+				if (isNaN(votesPerDollar) || Number.isNaN(votesPerDollar) || votesPerDollar === Infinity || votesPerDollar === -Infinity){
+					return "rgba(" + 4 + ", 0, 0, 0.2)";  //Gives a default value for buggy 'votePerDollar' math.
 				} else if (votesPerDollar < 0){
-					console.log(votesPerDollar);
-					return Math.abs(votesPerDollar);
+					console.log("[circle:fill] Votes Per Dollar: " + votesPerDollar);
+					return Math.abs(votesPerDollar);	// If the result of 'votesPerDollar' is negative, return a positive num
 				} else {
 					return "rgba(" + Math.floor(votesPerDollar * 3.5) + ", 0, 0, 0.2)";
 				}
@@ -187,7 +170,6 @@ function drawGraphic(dataset){
 
 			d3.select(this)
 				.attr("fill", "rgba(200, 20, 20, 0.7)");
-
 			d3.select("#tooltip")
 				.style("left", xPosition + "px")
 				.style("top", yPosition - padding/4 + "px")
@@ -200,7 +182,7 @@ function drawGraphic(dataset){
 				.select("#value3")
 				.text( function(){
 					var ratio = d[1]/d[0];
-					if (ratio === Infinity){
+					if (ratio === Infinity){ // Resolves the divide by zero issue with 'ratio'
 						return 0;
 					} else {
 					return addCommas(Math.floor(ratio));
@@ -216,20 +198,18 @@ function drawGraphic(dataset){
 
 			d3.select(this)
 				.attr("fill", function(d) {
-
 				votesPerDollar = rScaleX(d[1]/scaleY(d[0]));
 
-				if (isNaN(votesPerDollar) || Number.isNaN(votesPerDollar) || votesPerDollar === Infinity){
-					return "rgba(" + 4 + ", 0, 0, 0.2)";
+				if (isNaN(votesPerDollar) || Number.isNaN(votesPerDollar) || votesPerDollar === Infinity || votesPerDollar === -Infinity){
+					return "rgba(" + 4 + ", 0, 0, 0.2)"; //Gives a default value for buggy 'votePerDollar' math.
 				} else if (votesPerDollar < 0){
-					console.log(votesPerDollar);
-					return Math.abs(votesPerDollar);
+					console.log("[circle:mouseout] Votes Per Dollar: " + votesPerDollar);
+					return Math.abs(votesPerDollar);	// If the result of 'votesPerDollar' is negative, return a positive num
 				} else {
 					return "rgba(" + Math.floor(votesPerDollar * 3.5) + ", 0, 0, 0.2)";
 				}
 			});
 		});
-
 
 	svg.append("g")
 		.attr("class", "axis")
@@ -249,40 +229,66 @@ function drawGraphic(dataset){
 
 
 // variable for setting the string to be searched for in _.where. This is how the full list of legislators can be accessed:
+// var selectedName = "John Boehner (R)";
 var selectedName = "John Boehner (R)";
+var issuesCount;
+
 
 d3.csv('../assets/money-votes_All.csv', function(error, data) {
-	
 	if (error) {
 		console.log(error);
 	} else {
 		// Logs the data as it came in:
 		// console.log(data);
-	}
 
-	oldDataset = _.where(data, {person: selectedName});
-	// console.log(oldDataset);
-	name = oldDataset[0].person.replace(/\([RD]\)/g, '').trim();
 
-	for (var i = 0; i < oldDataset.length; i++) {
-		var tempNum1 = parseInt(oldDataset[i].votes);
-		var tempNum2 = parseInt(oldDataset[i].money.replace(/[\$,]/g, ''));
+	filteredDataset = _.where(data, {person: selectedName});
+	console.log(filteredDataset);
 
-		var tempCategory = oldDataset[i]["industry name"];
-		if (isNaN(tempNum1) || Number.isNaN(tempNum1) || isNaN(tempNum2) || Number.isNaN(tempNum2) ){
-			console.log("Error found at entry " + i + ": " + tempNum1 + ", " + tempNum2);
+	// issuesCount = _.where(filteredDataset, {});
+	var votesCount = [];
+	var contributionsCount = [];
+
+	name = filteredDataset[0].person.replace(/\([RD]\)/g, '').trim();
+
+	for (var i = 0; i < filteredDataset.length; i++) {
+		var tempVotes = parseInt(filteredDataset[i].votes);
+		var tempMoney = parseInt(filteredDataset[i].money.replace(/[\$,]/g, ''));
+		var tempCategory = filteredDataset[i]["industry name"];
+
+		if (isNaN(tempVotes) || Number.isNaN(tempVotes) || isNaN(tempMoney) || Number.isNaN(tempMoney) ){
+			console.log("Error found at entry " + i + ": " + tempVotes + ", " + tempMoney);
 		} else {
-			dataset.push([tempNum1, tempNum2, tempCategory]);
+			votesCount.push(Math.abs(tempVotes));
+			contributionsCount.push(tempMoney);
+			dataset.push([tempVotes, tempMoney, tempCategory]);
 		}
 	}
 	// Logs the data as it was processed:
 	// console.log(dataset);
-
+	$('#totalVotes').text(addCommas(votesCount.sum()));
+	$('#totalContributions').text(addCommas(contributionsCount.sum()));
 	// Calls the function that gets info about the member of Congress:
 	getCongressMemberData(name);
 
 	// processCRPCategories();
 	// Calls the function that visualizes the processed data:
 	drawGraphic(dataset);
-
+	
+	}
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
